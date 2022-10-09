21945cbedaccfef599b1afff51e624fa227f1895
Danish <danish.117@outlook.com>
2022-10-09 19:01:52 +0500

    Add project files.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        private const string UserName = "guest";
+        private const string Password = "guest";
+        private const string HostName = "localhost";
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = HostName,
+                UserName = UserName,
+                Password = Password,
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            // accept only one unack-ed message at a time
+            // uint prefetchSize, ushort prefetchCount, bool global
+            channel.BasicQos(0, 1, false);
+            MessageReceiver messageReceiver = new MessageReceiver(channel);
+            channel.BasicConsume("test", false, messageReceiver);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQPublisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "guest";
+            string Password = "guest";
+            string HostName = "localhost";
+
+            var connectionFactory = new ConnectionFactory()
+            {
+                UserName = UserName,
+                Password = Password,
+                HostName = HostName
+            };
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            // Create Exchange
+            //model.ExchangeDeclare("demoExchange", ExchangeType.Direct);
+            //Console.WriteLine("Creating Exchange");
+
+            // Create Queue
+            //model.QueueDeclare("demoqueue", true, false, false, null);
+            //Console.WriteLine("Creating Queue");
+
+            // Bind Queue to Exchange
+            //model.QueueBind("test", "test-exchange", "testkey");
+            //Console.WriteLine("Creating Binding");
+
+            var properties = model.CreateBasicProperties();
+            properties.Persistent = false;
+
+            byte[] messagebuffer = Encoding.Default.GetBytes("Test Message");
+            model.BasicPublish("test-exchange","testkey", properties, messagebuffer);
+            Console.WriteLine("Message Sent");
+        }
+    }
+}
